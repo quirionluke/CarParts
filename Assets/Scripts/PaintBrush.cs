c66be945817477636ed700b593911b55790fc012
quirionluke <quirion.l@husky.neu.edu>
2022-09-20 15:41:37 -0400

    Fixed bug where painting would paint on multiple surfaces

--- Assets/Scripts/PaintBrush.cs
+++ Assets/Scripts/PaintBrush.cs
@@ -2,21 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// PaintBrush treats the mouse like a paintbrush, however it only allows painting on objects with a mesh collider and tagged "Paintable"
+// PaintBrush treats the mouse like a paintbrush, however it only allows painting on objects with a non-convex mesh collider and tagged "Paintable"
 public class PaintBrush : MonoBehaviour
 {
     public Color paintColor = Color.green;
 
-    Texture2D texture;
+    public int textureRes = 64;
 
     private void Start()
     {
-        texture = new Texture2D(1, 1);
-        texture.SetPixel(0, 0, Color.clear);
 
-        // the actual size of the texture
-        texture.Resize(64, 64);
-        texture.Apply();
     }
 
     private void Update()
@@ -44,17 +39,49 @@ public class PaintBrush : MonoBehaviour
                     if (mr == null || mr.material == null || meshCollider == null)
                         return;
 
-                    if (mr.materials.Length != 0)
+                    foreach (Material m in mr.materials)
                     {
+                        if (!m.mainTexture)
+                        {
+                            m.mainTexture = CreateTexture(textureRes, textureRes);
+                        }
+
+
+                        Texture2D tex = m.mainTexture as Texture2D;
+                        Vector2 pixelUV = hit.textureCoord;
+
+                        pixelUV.x *= tex.width;
+                        pixelUV.y *= tex.height;
+
+                        tex.SetPixel((int)pixelUV.x, (int)pixelUV.y, paintColor);
+                        tex.Apply();
+                    }
+                    
+                }
+                else
+                {
+                    foreach (MeshRenderer mr in hit.transform.GetComponentsInChildren<MeshRenderer>())
+                    {
+
+                        MeshCollider meshCollider = mr.gameObject.GetComponent<MeshCollider>();
+
+
+                        if (mr == null || mr.material == null || meshCollider == null)
+                            return;
+
+                        if (hit.collider != meshCollider)
+                            continue;
+
                         foreach (Material m in mr.materials)
                         {
+
                             if (!m.mainTexture)
                             {
-                                m.mainTexture = texture;
+                                m.mainTexture = CreateTexture(textureRes, textureRes);
                             }
 
-
                             Texture2D tex = m.mainTexture as Texture2D;
+
                             Vector2 pixelUV = hit.textureCoord;
 
                             pixelUV.x *= tex.width;
@@ -63,44 +90,24 @@ public class PaintBrush : MonoBehaviour
                             tex.SetPixel((int)pixelUV.x, (int)pixelUV.y, paintColor);
                             tex.Apply();
                         }
-                    }
-                }
-                else
-                {
-                    foreach (MeshRenderer mr in hit.transform.GetComponentsInChildren<MeshRenderer>())
-                    {
-                        mr.material.mainTexture = texture;
-
-                        MeshCollider meshCollider = mr.gameObject.GetComponent<MeshCollider>();
-
-
-                        if (mr == null || mr.material == null || mr.material.mainTexture == null || meshCollider == null)
-                            return;
-
-                        if (mr.materials.Length != 0)
-                        {
-                            foreach (Material m in mr.materials)
-                            {
-                                if (!m.mainTexture)
-                                {
-                                    m.mainTexture = texture;
-                                }
-
-                                Texture2D tex = m.mainTexture as Texture2D;
-                                Vector2 pixelUV = hit.textureCoord;
-
-                                pixelUV.x *= tex.width;
-                                pixelUV.y *= tex.height;
-
-                                tex.SetPixel((int)pixelUV.x, (int)pixelUV.y, paintColor);
-                                tex.Apply();
-                            }
-                        }
+                        
                     }
                 }
             }
             
         }
+    }
+
+    public Texture2D CreateTexture(int width, int height)
+    {
+        Texture2D texture = new Texture2D(1, 1);
+        texture.SetPixel(0, 0, Color.clear);
+
+        // the actual size of the texture
+        texture.Resize(width, height);
+        texture.Apply();
+
+        return texture;
     }
 
 
